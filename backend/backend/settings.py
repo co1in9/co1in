78f1ff78576811d9a1ab7f4191df1cc807ee5e73
Colin Chen <me@co1in.com>
2019-07-06 15:50:53 +0800

    allow all host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0puk@6oaeqqri!k88du7elgcw40gj$vcrt9ab$^lyc5vg$k+8_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
